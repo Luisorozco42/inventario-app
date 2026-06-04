2e4e020c1ec220ff9914ac766d0585b0a1a24b20
Luisorozco42 <lorozcolazo@gmail.com>
2026-06-04 16:44:18 -0600

    adding a product works

--- src/app/agregar-producto/agregar-producto.ts
+++ src/app/agregar-producto/agregar-producto.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Producto } from '../producto';
+import { ProductoService } from '../producto.service';
+import { Router } from '@angular/router';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-agregar-producto',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './agregar-producto.html',
 })
-export class AgregarProducto {}
+export class AgregarProducto {
+  producto: Producto = new Producto();//Estamos usando el concepto de two way data binding, por lo que el objeto producto se va a llenar con los datos que el usuario ingrese en el formulario
+
+  private productoServicio = inject(ProductoService);
+  private router = inject(Router);
+
+  onSubmit() {
+    this.guardarProducto();
+  }
+
+  guardarProducto(){
+    this.productoServicio.agregarProducto(this.producto).subscribe({
+      next: (datos) =>{
+        this.irListaProductos();
+      },
+      error: (error: any) => console.log(error)
+    });
+  }
+
+  irListaProductos(){
+    this.router.navigate(['/productos']);
+  }
+}
